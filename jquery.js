6d35697cd2526a42aebc404c3c42adb96a6c0f7b
Agnieszka Kurek <agniechakurek@gmail.com>
2016-06-01 13:14:22 +0200

    using ID selector

--- jquery.js
+++ jquery.js
@@ -2,9 +2,16 @@ $(document).ready(function(){
   
   
   
-  $('div').css('background-color', '#38bode');
+  //$('div').css('background-color', '#38bode');
   
+   // $('div').each(function () {
+           //alert($(this).html());
+    //});
+    
+    //var coll = $('div, span');
+   // alert(coll.length);
   
+  alert($('#TestDiv').html());
   
   
   
