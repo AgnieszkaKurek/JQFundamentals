d02f3004820662def1f2029bc98e49edd3787dcc
Agnieszka Kurek <agniechakurek@gmail.com>
2016-06-07 14:01:26 +0200

    use some selector

--- jquery.js
+++ jquery.js
@@ -16,10 +16,16 @@ $(document).ready(function(){
     //alert($('div[title]').length);
      //var divs =  $('div[title = "Div Title"]');
     // alert(divs.length);
-     var inputs =  $('inputs[title = "text"]');
-     inputs.css('background-color','#ccff66');
-  
-  
+     //var inputs =  $('inputs[title = "text"]');
+     //inputs.css('background-color','#ccff66');
+       //$(':input').each(function(){
+          //var elem = $(this);
+          //alert(elem.val());
+          
+       //});
+       
+       alert($('div:contains("my div")').html());
+      
   
   
 });
